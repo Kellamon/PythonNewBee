d7073678ccf000cce102f34501ee9bdc30aff2ec
Kellamon <fqvxiwzz5iym@mail.ru>
2023-02-07 15:06:40 +0300

    Добавлен вариант решения 3 задачи в 4 семинаре

--- 4th_Sem.py/Tasks.py
+++ 4th_Sem.py/Tasks.py
@@ -2,33 +2,33 @@
 # сколько раз каждый символ уже встречался.
 # Количество повторов добавляется к символам с помощью постфикса формата _n
 
-string = 'd g h t r g r h t j h b v f d s d f'
-print(string)
-my_list = string.split()
-my_dict = {}
-new_list = []
-for letter in my_list:
-    my_dict[letter] = my_dict.get(letter, 0) + 1   # используем метод гет по ключу letter
-    if my_dict.get(letter) > 1:
-        new_list.append(letter + '_' + str(my_dict.get(letter)))
-    else:
-        new_list.append(letter)
-print(' '.join(new_list))
+# string = 'd g h t r g r h t j h b v f d s d f'
+# print(string)
+# my_list = string.split()
+# my_dict = {}
+# new_list = []
+# for letter in my_list:
+#     my_dict[letter] = my_dict.get(letter, 0) + 1   # используем метод гет по ключу letter
+#     if my_dict.get(letter) > 1:
+#         new_list.append(letter + '_' + str(my_dict.get(letter)))
+#     else:
+#         new_list.append(letter)
+# print(' '.join(new_list))
 
 
-# # Пользователь вводит текст(строк).
-# # Словом считается последовательность непробельных символов идущих подряд,
-# # слова разделены одним или большим числом пробелов.
-# # Определите, сколько различных слов содержится в этом тексте.
+# # # Пользователь вводит текст(строк).
+# # # Словом считается последовательность непробельных символов идущих подряд,
+# # # слова разделены одним или большим числом пробелов.
+# # # Определите, сколько различных слов содержится в этом тексте.
 
-string = 'sdsd sdsd sdsdf   fsdfg ggk    lgg lkjlkl sdsd'
-my_list = string.split(' ') # если убрать пробел тут, то строчки с ремув не нужны
-print(my_list)
-my_dict = set(my_list)
-print(my_dict)
-my_dict.remove('') # убрали пробелы в строке
-print(my_dict)
-print(f'В нашем тексте {len(my_dict)} уникальных слов(а)')
+# string = 'sdsd sdsd sdsdf   fsdfg ggk    lgg lkjlkl sdsd'
+# my_list = string.split(' ') # если убрать пробел тут, то строчки с ремув не нужны
+# print(my_list)
+# my_dict = set(my_list)
+# print(my_dict)
+# my_dict.remove('') # убрали пробелы в строке
+# print(my_dict)
+# print(f'В нашем тексте {len(my_dict)} уникальных слов(а)')
 
 # Дана последовательность чисел. 
 # Получить список уникальных элементов заданной последовательнсоти
@@ -47,3 +47,5 @@ for i in my_dict:
         new_list.append(i)
         
 print(new_list)
+
+print([letter for letter in num if num.count(letter) == 1]) # вывод через лист сравнения(более эффективный вариант решения задачи)
